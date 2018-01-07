2e557af6f25a884f6e2146df6f59aab8bf10223f
Nathan Burgess <nathan@3playmedia.com>
2018-01-07 10:56:22 -0500

    Added dotenv

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'sinatra', '~> 1.4', '>= 1.4.7'
 gem 'curb', '~> 0.9.3'
+gem 'dotenv', '~> 2.1', '>= 2.1.1'
--- server.rb
+++ server.rb
@@ -1,12 +1,20 @@
-# TODO: move to gem file
-# require "sinatra"
-# require "curb"
+require 'dotenv/load'
+require 'sinatra'
+require 'curb'
 
 get "/" do
-  "It's alive!!!"
+  ENV['TESTING']
 end
 
 get "/movie_search" do
+  endpoint = "https://api.themoviedb.org/3/search/movie?api_key="
+  endpoint += ENV["TMDB_V3_KEY"]
+  endpoint += "&query="
+  # Uri encode?
+  endpoint += params[:query]
+  http = Curl.get(endpoint)
+  # to json?
+  return http.body_str
   # "https://api.themoviedb.org/3/search/movie?api_key=" + KeyConfig.TMDB_V3_KEY + "&query=" + encodeURI(this.props.searchValue);
 end
 

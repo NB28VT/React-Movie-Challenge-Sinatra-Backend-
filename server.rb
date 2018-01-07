151f379bbcd6e76118cae5cea389c641eeae7eed
Nathan Burgess <nathan@3playmedia.com>
2018-01-07 11:43:27 -0500

    Built out endpoints

--- server.rb
+++ server.rb
@@ -1,33 +1,23 @@
 require 'dotenv/load'
 require 'sinatra'
-require 'curb'
-
-get "/" do
-  ENV['TESTING']
-end
+require 'net/http'
 
 get "/movie_search" do
-  endpoint = "https://api.themoviedb.org/3/search/movie?api_key="
-  endpoint += ENV["TMDB_V3_KEY"]
-  endpoint += "&query="
-  # Uri encode?
-  endpoint += params[:query]
-  http = Curl.get(endpoint)
-  # to json?
-  return http.body_str
-  # "https://api.themoviedb.org/3/search/movie?api_key=" + KeyConfig.TMDB_V3_KEY + "&query=" + encodeURI(this.props.searchValue);
+  uri = URI("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_V3_KEY']}&query=#{params[:query]}")
+  Net::HTTP.get(uri)
 end
 
 get "/movie_data" do
-  # "https://api.themoviedb.org/3/movie/" + movieData.id + "/credits?api_key=" + KeyConfig.TMDB_V3_KEY;
+  uri = URI("https://api.themoviedb.org/3/movie/#{params[:movie_id]}/credits?api_key=#{ENV['TMDB_V3_KEY']}")
+  Net::HTTP.get(uri)
 end
-
 
 get "/movie_thumbnail" do
-  # "https://image.tmdb.org/t/p/w500" +  this.props.movieData.poster_path + "?api_key=" + KeyConfig.TMDB_V3_KEY;
+  uri = URI("https://image.tmdb.org/t/p/w500#{params[:poster_path]}?api_key=#{ENV['TMDB_V3_KEY']}")
+  Net::HTTP.get(uri)
 end
 
-
 get "/cast_thumbnail" do
-  # thumbnailUrl = "https://image.tmdb.org/t/p/w500" +  this.props.castMember.profile_path + "?api_key=" + KeyConfig.TMDB_V3_KEY;
+  uri = URI("https://image.tmdb.org/t/p/w500#{params[:profile_picture_path]}?api_key=#{ENV['TMDB_V3_KEY']}")
+  Net::HTTP.get(uri)
 end

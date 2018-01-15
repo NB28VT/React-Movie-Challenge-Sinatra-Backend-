c439b69d0473cb35aac5eba7762976ea244f7d30
Nathan Burgess <nathan@3playmedia.com>
2018-01-15 11:03:26 -0500

    Try image headers

--- server.rb
+++ server.rb
@@ -12,27 +12,35 @@ configure :production do
   ALLOWED_ORIGIN = 'https://whos-init.herokuapp.com'
 end
 
-before do
-   content_type :json
-   headers 'Access-Control-Allow-Origin' => ALLOWED_ORIGIN
-end
+# before do
+#    content_type :json
+#    headers 'Access-Control-Allow-Origin' => ALLOWED_ORIGIN
+# end
 
 get "/movie_search" do
+  content_type :json
+  headers 'Access-Control-Allow-Origin' => ALLOWED_ORIGIN
   uri = URI("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_V3_KEY']}&query=#{params[:query]}")
   Net::HTTP.get(uri)
 end
 
 get "/movie_data" do
+  content_type :json
+  headers 'Access-Control-Allow-Origin' => ALLOWED_ORIGIN
   uri = URI("https://api.themoviedb.org/3/movie/#{params[:movie_id]}/credits?api_key=#{ENV['TMDB_V3_KEY']}")
   Net::HTTP.get(uri)
 end
 
 get "/movie_thumbnail" do
+  content_type :jpg
+  headers 'Access-Control-Allow-Origin' => ALLOWED_ORIGIN
   uri = URI("https://image.tmdb.org/t/p/w500/#{params[:poster_path]}.jpg?api_key=#{ENV['TMDB_V3_KEY']}")
   Net::HTTP.get(uri)
 end
 
 get "/cast_thumbnail" do
+  content_type :jpg
+  headers 'Access-Control-Allow-Origin' => ALLOWED_ORIGIN
   uri = URI("https://image.tmdb.org/t/p/w500/#{params[:profile_picture_path]}.jpg?api_key=#{ENV['TMDB_V3_KEY']}")
   Net::HTTP.get(uri)
 end
